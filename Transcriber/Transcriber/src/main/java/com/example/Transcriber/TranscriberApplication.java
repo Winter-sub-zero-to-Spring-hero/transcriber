8f2abe16ef62609836d04e5c1558fadf8f3e7aaf
Cosmin - Sebastian <96211653+CosminB24@users.noreply.github.com>
2023-06-09 16:59:16 +0300

    Add files via upload
    + project structure

---
+++ Transcriber/Transcriber/src/main/java/com/example/Transcriber/TranscriberApplication.java
@@ -0,0 +1,13 @@
+package com.example.Transcriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscriberApplication.class, args);
+	}
+
+}
---
+++ Transcriber/Transcriber/src/test/java/com/example/Transcriber/TranscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Transcriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscriberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
